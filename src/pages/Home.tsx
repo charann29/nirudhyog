6163a2d62bab7747b00038088affabe6b00a0a33
saicherry93479 <saicherry93479@gmail.com>
2024-07-30 17:55:39 +0530

    footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import FAQ from "../components/FAQ";
 import HomeHero from "../components/HomeHero";
-import NavBar from "../components/Header";
 import Metrics from "../components/Metrics";
 import Courses from "../components/Courses";
 import ContactUs from "../components/ContactUs";
 import PricePlan from "../components/Pricing";
-import Footer from "../components/Footer";
 import MentorShip from "../components/MentorShip";
 
 const Home = () => {
